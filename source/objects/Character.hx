8bee56c7105b3999163588c5c32aa2c7be8f750c
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-14 05:22:03 -0500

    Update Character.hx

--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -190,7 +190,7 @@ class Character extends FlxSprite {
 				var animAnim:String = '' + anim.anim;
 				var animName:String = '' + anim.name;
 				var animFps:Int = anim.fps;
-				var animLoop:Bool = !!anim.loop; // Bruh
+				var animLoop:Bool = !!anim.loop;
 				var animIndices:Array<Int> = anim.indices;
 
 				if (!isAnimateAtlas) {
@@ -214,10 +214,12 @@ class Character extends FlxSprite {
 					addOffset(anim.anim, 0, 0);
 			}
 		}
+
 		#if flxanimate
 		if (isAnimateAtlas)
 			copyAtlasValues();
 		#end
+
 		// =======================================
 		// APPLY RECOLOR PRESET (SAFE, ONCE)
 		// =======================================
@@ -228,276 +230,282 @@ class Character extends FlxSprite {
 			if (map != null && map.keys().hasNext()) {
 				backend.RecolorUtil.recolor(this, map);
 			}
-			// trace('Loaded file to character ' + curCharacter);
 		}
+	}
 
-		override function update(elapsed:Float) {
-			if (isAnimateAtlas)
-				atlas.update(elapsed);
+	override function update(elapsed:Float) {
+		if (isAnimateAtlas)
+			atlas.update(elapsed);
 
-			if (debugMode
-				|| (!isAnimateAtlas && animation.curAnim == null)
-				|| (isAnimateAtlas && (atlas.anim.curInstance == null || atlas.anim.curSymbol == null))) {
-				super.update(elapsed);
-				return;
-			}
-
-			if (heyTimer > 0) {
-				var rate:Float = (PlayState.instance != null ? PlayState.instance.playbackRate : 1.0);
-				heyTimer -= elapsed * rate;
-				if (heyTimer <= 0) {
-					var anim:String = getAnimationName();
-					if (specialAnim && (anim == 'hey' || anim == 'cheer')) {
-						specialAnim = false;
-						dance();
-					}
-					heyTimer = 0;
-				}
-			} else if (specialAnim && isAnimationFinished()) {
-				specialAnim = false;
-				dance();
-			} else if (getAnimationName().endsWith('miss') && isAnimationFinished()) {
-				dance();
-				finishAnimation();
-			}
-
-			switch (curCharacter) {
-				case 'pico-speaker':
-					if (animationNotes.length > 0 && Conductor.songPosition > animationNotes[0][0]) {
-						var noteData:Int = 1;
-						if (animationNotes[0][1] > 2)
-							noteData = 3;
-
-						noteData += FlxG.random.int(0, 1);
-						playAnim('shoot' + noteData, true);
-						animationNotes.shift();
-					}
-					if (isAnimationFinished())
-						playAnim(getAnimationName(), false, false, animation.curAnim.frames.length - 3);
-			}
-
-			if (getAnimationName().startsWith('sing'))
-				holdTimer += elapsed;
-			else if (isPlayer)
-				holdTimer = 0;
-
-			if (!isPlayer
-				&& holdTimer >= Conductor.stepCrochet * (0.0011 #if FLX_PITCH / (FlxG.sound.music != null ? FlxG.sound.music.pitch : 1) #end) * singDuration) {
-				dance();
-				holdTimer = 0;
-			}
-
-			var name:String = getAnimationName();
-			if (isAnimationFinished() && hasAnimation('$name-loop'))
-				playAnim('$name-loop');
-
+		if (debugMode
+			|| (!isAnimateAtlas && animation.curAnim == null)
+			|| (isAnimateAtlas && (atlas.anim.curInstance == null || atlas.anim.curSymbol == null))) {
 			super.update(elapsed);
+			return;
 		}
 
-		inline public function isAnimationNull():Bool {
-			return !isAnimateAtlas ? (animation.curAnim == null) : (atlas.anim.curInstance == null || atlas.anim.curSymbol == null);
-		}
-
-		var _lastPlayedAnimation:String;
-		inline public function getAnimationName():String {
-			return _lastPlayedAnimation;
-		}
-
-		public function isAnimationFinished():Bool {
-			if (isAnimationNull())
-				return false;
-			return !isAnimateAtlas ? animation.curAnim.finished : atlas.anim.finished;
-		}
-
-		public function finishAnimation():Void {
-			if (isAnimationNull())
-				return;
-
-			if (!isAnimateAtlas)
-				animation.curAnim.finish();
-			else
-				atlas.anim.curFrame = atlas.anim.length - 1;
-		}
-
-		public function hasAnimation(anim:String):Bool {
-			return animOffsets.exists(anim);
-		}
-
-		public var animPaused(get, set):Bool;
-		private function get_animPaused():Bool {
-			if (isAnimationNull())
-				return false;
-			return !isAnimateAtlas ? animation.curAnim.paused : atlas.anim.isPlaying;
-		}
-		private function set_animPaused(value:Bool):Bool {
-			if (isAnimationNull())
-				return value;
-			if (!isAnimateAtlas)
-				animation.curAnim.paused = value;
-			else {
-				if (value)
-					atlas.pauseAnimation();
-				else
-					atlas.resumeAnimation();
-			}
-
-			return value;
-		}
-
-		public var danced:Bool = false;
-
-		/**
-		 * FOR GF DANCING SHIT
-		 */
-		public function dance() {
-			if (!debugMode && !skipDance && !specialAnim) {
-				if (danceIdle) {
-					danced = !danced;
-
-					if (danced)
-						playAnim('danceRight' + idleSuffix);
-					else
-						playAnim('danceLeft' + idleSuffix);
-				} else if (hasAnimation('idle' + idleSuffix))
-					playAnim('idle' + idleSuffix);
-			}
-		}
-
-		public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void {
-			specialAnim = false;
-			if (!isAnimateAtlas) {
-				animation.play(AnimName, Force, Reversed, Frame);
-			} else {
-				atlas.anim.play(AnimName, Force, Reversed, Frame);
-				atlas.update(0);
-			}
-			_lastPlayedAnimation = AnimName;
-
-			if (hasAnimation(AnimName)) {
-				var daOffset = animOffsets.get(AnimName);
-				offset.set(daOffset[0], daOffset[1]);
-			}
-			// else offset.set(0, 0);
-
-			if (curCharacter.startsWith('gf-') || curCharacter == 'gf') {
-				if (AnimName == 'singLEFT')
-					danced = true;
-				else if (AnimName == 'singRIGHT')
-					danced = false;
-
-				if (AnimName == 'singUP' || AnimName == 'singDOWN')
-					danced = !danced;
-			}
-		}
-
-		function loadMappedAnims():Void {
-			try {
-				var songData:SwagSong = Song.getChart('picospeaker', Paths.formatToSongPath(Song.loadedSongName));
-				if (songData != null)
-					for (section in songData.notes)
-						for (songNotes in section.sectionNotes)
-							animationNotes.push(songNotes);
-
-				TankmenBG.animationNotes = animationNotes;
-				animationNotes.sort(sortAnims);
-			} catch (e:Dynamic) {}
-		}
-
-		function sortAnims(Obj1:Array<Dynamic>, Obj2:Array<Dynamic>):Int {
-			return FlxSort.byValues(FlxSort.ASCENDING, Obj1[0], Obj2[0]);
-		}
-
-		public var danceEveryNumBeats:Int = 2;
-		private var settingCharacterUp:Bool = true;
-		public function recalculateDanceIdle() {
-			var lastDanceIdle:Bool = danceIdle;
-			danceIdle = (hasAnimation('danceLeft' + idleSuffix) && hasAnimation('danceRight' + idleSuffix));
-
-			if (settingCharacterUp) {
-				danceEveryNumBeats = (danceIdle ? 1 : 2);
-			} else if (lastDanceIdle != danceIdle) {
-				var calc:Float = danceEveryNumBeats;
-				if (danceIdle)
-					calc /= 2;
-				else
-					calc *= 2;
-
-				danceEveryNumBeats = Math.round(Math.max(calc, 1));
-			}
-			settingCharacterUp = false;
-		}
-
-		public function addOffset(name:String, x:Float = 0, y:Float = 0) {
-			animOffsets[name] = [x, y];
-		}
-
-		public function quickAnimAdd(name:String, anim:String) {
-			animation.addByPrefix(name, anim, 24, false);
-		}
-
-		// Atlas support
-		// special thanks ne_eo for the references, you're the goat!!
-		@:allow(states.editors.CharacterEditorState)
-		public var isAnimateAtlas(default, null):Bool = false;
-		#if flxanimate
-		public var atlas:FlxAnimate;
-		public override function draw() {
-			var lastAlpha:Float = alpha;
-			var lastColor:FlxColor = color;
-			if (missingCharacter) {
-				alpha *= 0.6;
-				color = FlxColor.BLACK;
-			}
-
-			if (isAnimateAtlas) {
-				if (atlas.anim.curInstance != null) {
-					copyAtlasValues();
-					atlas.draw();
-					alpha = lastAlpha;
-					color = lastColor;
-					if (missingCharacter && visible) {
-						missingText.x = getMidpoint().x - 150;
-						missingText.y = getMidpoint().y - 10;
-						missingText.draw();
-					}
+		if (heyTimer > 0) {
+			var rate:Float = (PlayState.instance != null ? PlayState.instance.playbackRate : 1.0);
+			heyTimer -= elapsed * rate;
+			if (heyTimer <= 0) {
+				var anim:String = getAnimationName();
+				if (specialAnim && (anim == 'hey' || anim == 'cheer')) {
+					specialAnim = false;
+					dance();
 				}
-				return;
+				heyTimer = 0;
 			}
-			super.draw();
-			if (missingCharacter && visible) {
+		} else if (specialAnim && isAnimationFinished()) {
+			specialAnim = false;
+			dance();
+		} else if (getAnimationName().endsWith('miss') && isAnimationFinished()) {
+			dance();
+			finishAnimation();
+		}
+
+		switch (curCharacter) {
+			case 'pico-speaker':
+				if (animationNotes.length > 0 && Conductor.songPosition > animationNotes[0][0]) {
+					var noteData:Int = 1;
+					if (animationNotes[0][1] > 2)
+						noteData = 3;
+
+					noteData += FlxG.random.int(0, 1);
+					playAnim('shoot' + noteData, true);
+					animationNotes.shift();
+				}
+				if (isAnimationFinished())
+					playAnim(getAnimationName(), false, false, animation.curAnim.frames.length - 3);
+		}
+
+		if (getAnimationName().startsWith('sing'))
+			holdTimer += elapsed;
+		else if (isPlayer)
+			holdTimer = 0;
+
+		if (!isPlayer
+			&& holdTimer >= Conductor.stepCrochet * (0.0011 #if FLX_PITCH / (FlxG.sound.music != null ? FlxG.sound.music.pitch : 1) #end) * singDuration) {
+			dance();
+			holdTimer = 0;
+		}
+
+		var name:String = getAnimationName();
+		if (isAnimationFinished() && hasAnimation('$name-loop'))
+			playAnim('$name-loop');
+
+		super.update(elapsed);
+	}
+
+	inline public function isAnimationNull():Bool {
+		return !isAnimateAtlas ? (animation.curAnim == null) : (atlas.anim.curInstance == null || atlas.anim.curSymbol == null);
+	}
+
+	var _lastPlayedAnimation:String;
+
+	inline public function getAnimationName():String {
+		return _lastPlayedAnimation;
+	}
+
+	public function isAnimationFinished():Bool {
+		if (isAnimationNull())
+			return false;
+		return !isAnimateAtlas ? animation.curAnim.finished : atlas.anim.finished;
+	}
+
+	public function finishAnimation():Void {
+		if (isAnimationNull())
+			return;
+
+		if (!isAnimateAtlas)
+			animation.curAnim.finish();
+		else
+			atlas.anim.curFrame = atlas.anim.length - 1;
+	}
+
+	public function hasAnimation(anim:String):Bool {
+		return animOffsets.exists(anim);
+	}
+
+	public var animPaused(get, set):Bool;
+
+	private function get_animPaused():Bool {
+		if (isAnimationNull())
+			return false;
+		return !isAnimateAtlas ? animation.curAnim.paused : atlas.anim.isPlaying;
+	}
+
+	private function set_animPaused(value:Bool):Bool {
+		if (isAnimationNull())
+			return value;
+		if (!isAnimateAtlas)
+			animation.curAnim.paused = value;
+		else {
+			if (value)
+				atlas.pauseAnimation();
+			else
+				atlas.resumeAnimation();
+		}
+
+		return value;
+	}
+
+	public var danced:Bool = false;
+
+	/**
+	 * FOR GF DANCING SHIT
+	 */
+	public function dance() {
+		if (!debugMode && !skipDance && !specialAnim) {
+			if (danceIdle) {
+				danced = !danced;
+
+				if (danced)
+					playAnim('danceRight' + idleSuffix);
+				else
+					playAnim('danceLeft' + idleSuffix);
+			} else if (hasAnimation('idle' + idleSuffix))
+				playAnim('idle' + idleSuffix);
+		}
+	}
+
+	public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void {
+		specialAnim = false;
+		if (!isAnimateAtlas) {
+			animation.play(AnimName, Force, Reversed, Frame);
+		} else {
+			atlas.anim.play(AnimName, Force, Reversed, Frame);
+			atlas.update(0);
+		}
+		_lastPlayedAnimation = AnimName;
+
+		if (hasAnimation(AnimName)) {
+			var daOffset = animOffsets.get(AnimName);
+			offset.set(daOffset[0], daOffset[1]);
+		}
+		// else offset.set(0, 0);
+
+		if (curCharacter.startsWith('gf-') || curCharacter == 'gf') {
+			if (AnimName == 'singLEFT')
+				danced = true;
+			else if (AnimName == 'singRIGHT')
+				danced = false;
+
+			if (AnimName == 'singUP' || AnimName == 'singDOWN')
+				danced = !danced;
+		}
+	}
+
+	function loadMappedAnims():Void {
+		try {
+			var songData:SwagSong = Song.getChart('picospeaker', Paths.formatToSongPath(Song.loadedSongName));
+			if (songData != null)
+				for (section in songData.notes)
+					for (songNotes in section.sectionNotes)
+						animationNotes.push(songNotes);
+
+			TankmenBG.animationNotes = animationNotes;
+			animationNotes.sort(sortAnims);
+		} catch (e:Dynamic) {}
+	}
+
+	function sortAnims(Obj1:Array<Dynamic>, Obj2:Array<Dynamic>):Int {
+		return FlxSort.byValues(FlxSort.ASCENDING, Obj1[0], Obj2[0]);
+	}
+
+	public var danceEveryNumBeats:Int = 2;
+
+	private var settingCharacterUp:Bool = true;
+
+	public function recalculateDanceIdle() {
+		var lastDanceIdle:Bool = danceIdle;
+		danceIdle = (hasAnimation('danceLeft' + idleSuffix) && hasAnimation('danceRight' + idleSuffix));
+
+		if (settingCharacterUp) {
+			danceEveryNumBeats = (danceIdle ? 1 : 2);
+		} else if (lastDanceIdle != danceIdle) {
+			var calc:Float = danceEveryNumBeats;
+			if (danceIdle)
+				calc /= 2;
+			else
+				calc *= 2;
+
+			danceEveryNumBeats = Math.round(Math.max(calc, 1));
+		}
+		settingCharacterUp = false;
+	}
+
+	public function addOffset(name:String, x:Float = 0, y:Float = 0) {
+		animOffsets[name] = [x, y];
+	}
+
+	public function quickAnimAdd(name:String, anim:String) {
+		animation.addByPrefix(name, anim, 24, false);
+	}
+
+	// Atlas support
+	// special thanks ne_eo for the references, you're the goat!!
+	@:allow(states.editors.CharacterEditorState)
+	public var isAnimateAtlas(default, null):Bool = false;
+	#if flxanimate
+	public var atlas:FlxAnimate;
+
+	public override function draw() {
+		var lastAlpha:Float = alpha;
+		var lastColor:FlxColor = color;
+		if (missingCharacter) {
+			alpha *= 0.6;
+			color = FlxColor.BLACK;
+		}
+
+		if (isAnimateAtlas) {
+			if (atlas.anim.curInstance != null) {
+				copyAtlasValues();
+				atlas.draw();
 				alpha = lastAlpha;
 				color = lastColor;
-				missingText.x = getMidpoint().x - 150;
-				missingText.y = getMidpoint().y - 10;
-				missingText.draw();
+				if (missingCharacter && visible) {
+					missingText.x = getMidpoint().x - 150;
+					missingText.y = getMidpoint().y - 10;
+					missingText.draw();
+				}
 			}
+			return;
 		}
-
-		public function copyAtlasValues() {
-			@:privateAccess
-			{
-				atlas.cameras = cameras;
-				atlas.scrollFactor = scrollFactor;
-				atlas.scale = scale;
-				atlas.offset = offset;
-				atlas.origin = origin;
-				atlas.x = x;
-				atlas.y = y;
-				atlas.angle = angle;
-				atlas.alpha = alpha;
-				atlas.visible = visible;
-				atlas.flipX = flipX;
-				atlas.flipY = flipY;
-				atlas.shader = shader;
-				atlas.antialiasing = antialiasing;
-				atlas.colorTransform = colorTransform;
-				atlas.color = color;
-			}
+		super.draw();
+		if (missingCharacter && visible) {
+			alpha = lastAlpha;
+			color = lastColor;
+			missingText.x = getMidpoint().x - 150;
+			missingText.y = getMidpoint().y - 10;
+			missingText.draw();
 		}
-
-		public override function destroy() {
-			atlas = FlxDestroyUtil.destroy(atlas);
-			super.destroy();
-		}
-		#end
 	}
+
+	public function copyAtlasValues() {
+		@:privateAccess
+		{
+			atlas.cameras = cameras;
+			atlas.scrollFactor = scrollFactor;
+			atlas.scale = scale;
+			atlas.offset = offset;
+			atlas.origin = origin;
+			atlas.x = x;
+			atlas.y = y;
+			atlas.angle = angle;
+			atlas.alpha = alpha;
+			atlas.visible = visible;
+			atlas.flipX = flipX;
+			atlas.flipY = flipY;
+			atlas.shader = shader;
+			atlas.antialiasing = antialiasing;
+			atlas.colorTransform = colorTransform;
+			atlas.color = color;
+		}
+	}
+
+	public override function destroy() {
+		atlas = FlxDestroyUtil.destroy(atlas);
+		super.destroy();
+	}
+	#end
+}
